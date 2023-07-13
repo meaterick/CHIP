cfe927de7f60386afcd94a2f767a3999a96c1ed2
meaterick <89893748+meaterick@users.noreply.github.com>
2023-07-13 16:53:34 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -27,52 +27,49 @@ class _Homeapge extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              child: const DecoratedBox(
-                decoration: BoxDecoration(
-                    color: Color(0xff0A3459)
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 20,
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Image.asset("lib/images/school_image1.png", width: 72,height: 72,fit: BoxFit.cover,),
-                        const Column(
-                          children: [
-                            Text(""),
-                            Text("")
-                          ],
-                        )
-                      ],
-                    ),
-                  ),//banner
-                  ElevatedButton(
-                    onPressed: () {
+        body: Container(
+          color: const Color(0xff0A3459),
+          child: Center(
+            child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width / 5,
+                                    height: MediaQuery.of(context).size.height / 3,
+                                    child: Image.asset("lib/images/school_image1.png", width: 72,height: 72,fit: BoxFit.cover,)
+                                ),
+                                const Column(
+                                  children: [
+                                    Text("소통부터 시작하는 학교생활"),
+                                    Text("Ask School")
+                                  ],
+                                )
+                              ],
+                            ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width / 10,
+                            child: ElevatedButton(
+                              onPressed: () {
 
-                    },
-                    child: const Text(
-                      'Elevated Button',
-                      style: TextStyle(fontSize: 40),
+                              },
+                              child: const Text(
+                                'Elevated Button',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ],
+          ),
         )
-      )
     );
   }
 }

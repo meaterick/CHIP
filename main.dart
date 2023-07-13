690b36df116d748f4cc5ce7d3a8c34c15502ca5d
meaterick <89893748+meaterick@users.noreply.github.com>
2023-07-14 06:33:02 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,46 +29,118 @@ class _Homeapge extends State<Homepage> {
         body: Container(
           color: const Color(0xff0A3459),
           child: Center(
-            child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 3,
-                    child: Container(
-                      color: Colors.white,
-                      child: Column(
-                        children: [
-                            Row(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 4,
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            child: Image.asset("lib/images/school_image1.png", width: 72,height: 72,fit: BoxFit.cover,)
+                        ),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
                               children: [
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width / 5,
-                                    height: MediaQuery.of(context).size.height / 3,
-                                    child: Image.asset("lib/images/school_image1.png", width: 72,height: 72,fit: BoxFit.cover,)
-                                ),
-                                const Column(
-                                  children: [
-                                    Text("소통부터 시작하는 학교생활"),
-                                    Text("Ask School")
-                                  ],
-                                )
+                                Text("소통부터 시작하는 학교생활", style: TextStyle(fontSize: 20)),
+                                Text("Ask School", style: TextStyle(fontSize: 30))
                               ],
                             ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width / 10,
-                            child: ElevatedButton(
-                              onPressed: () {
-
-                              },
-                              child: const Text(
-                                'Elevated Button',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 20,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  height: MediaQuery.of(context).size.width / 7,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white, // Background color
+                    ),
+                    onPressed: () {
+
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ),
         )
+    );
+  }
+}
+
+class Basic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: BasicPage()
+    );
+  }
+}
+class BasicPage extends StatefulWidget {
+  @override
+  State<BasicPage> createState() => _BasicPage();
+}
+class _BasicPage extends State<BasicPage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold
+  );
+
+  final List<Widget> _widgetOptions = <Widget>[
+    MySellPostPage(),
+    MyPostPage(),
+    MyPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_snippet),
+            label: '나의 판매글',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: '마이페이지',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightGreen,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
